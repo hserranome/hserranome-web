ec45848c7288bd856329a934f386ebe8bd09c004
Hendy Serrano <hendy@hserrano.me>
2024-08-31 16:42:23 +0200

    change: page title description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "hserrano.me";
-export const SITE_DESCRIPTION = "hserrano personal website";
+export const SITE_TITLE = "Hendy Serrano";
+export const SITE_DESCRIPTION = "Personal website";
