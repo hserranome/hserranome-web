a378a376a02a0a4377356d5c565324b8060c95b8
Hendy Serrano <hendy@hserrano.me>
2024-08-31 18:59:45 +0200

    change: use astro transitions instead of htmx (styles issues)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
-import htmx from "astro-htmx";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://hserrano.me",
-	integrations: [mdx(), sitemap(), tailwind(), htmx()],
+	integrations: [mdx(), sitemap(), tailwind()],
 });
