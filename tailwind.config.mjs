31704ef4209102ca9b01ca386895463ef546b430
Hendy Serrano <hendy@hserrano.me>
2024-09-20 22:39:40 +0200

    change: font, dark theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
 	theme: {
 		extend: {
 		  fontFamily: {
-			sans: ['Courier Prime', ...defaultTheme.fontFamily.sans],
+			sans: ['Inter', ...defaultTheme.fontFamily.sans],
 		  },
 		},
 	  },
